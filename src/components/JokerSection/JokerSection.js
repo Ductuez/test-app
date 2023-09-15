8524de479d5b632e62bf6528ba9c9da31b6d3b8d
TueHo <hoductue@gmail.com>
2023-09-15 18:02:50 +0700

    refactor code

--- src/components/JokerSection/JokerSection.js
+++ src/components/JokerSection/JokerSection.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 
+import { contentJoke } from "./data";
 import "./JokerSection.css";
 
 const JokerSection = () => {
@@ -9,37 +10,18 @@ const JokerSection = () => {
   const [likePost, setLikePost] = useState([]);
   const [ready, setReady] = useState(false);
 
-  const contentJoker = [
-    {
-      content: ` A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on.",
-
-    The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
-    
-    The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."`,
-    },
-    {
-      content: `Teacher: "Kids,what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!"`,
-    },
-    {
-      content: `The teacher asked Jimmy, "Why is your cat at school today Jimmy?" Jimmy replied crying, "Because I heard my daddy tell my mommy, 'I am going to eat that pussy once Jimmy leaves for school today!'"`,
-    },
-    {
-      content: `A housewife, an accountant and a lawyer were asked "How much is 2+2?" The housewife replies: "Four!". The accountant says: "I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time." The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?"`,
-    },
-  ];
-
   const handleFunny = () => {
     if (count === -1) return;
 
     const likePostPresent = likePost;
     likePostPresent.push({
-      content: contentJoker[count].content,
+      content: contentJoke[count].content,
       like: true,
     });
 
     setCount((prevState) => {
       const countPresent = prevState || 0;
-      if (countPresent >= contentJoker.length - 1) {
+      if (countPresent >= contentJoke.length - 1) {
         setLikePost(likePostPresent);
         return -1;
       } else {
@@ -50,8 +32,29 @@ const JokerSection = () => {
     });
   };
 
+  const handleNotFunny = () => {
+    if (count === -1) return;
+    const likePostPresent = likePost;
+    likePostPresent.push({
+      content: contentJoke[count].content,
+      like: false,
+    });
+
+    setCount((prevState) => {
+      const countPresent = prevState;
+      if (countPresent >= contentJoke.length - 1) {
+        setLikePost(likePostPresent);
+
+        return -1;
+      } else {
+        setLikePost(likePostPresent);
+
+        return countPresent + 1;
+      }
+    });
+  };
+
   const saveLocalStorege = () => {
-    setReady(true);
     localStorage.setItem(
       "count",
       JSON.stringify({
@@ -88,28 +91,6 @@ const JokerSection = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [count]);
 
-  const handleNotFunny = () => {
-    if (count === -1) return;
-    const likePostPresent = likePost;
-    likePostPresent.push({
-      content: contentJoker[count].content,
-      like: false,
-    });
-
-    setCount((prevState) => {
-      const countPresent = prevState;
-      if (countPresent >= contentJoker.length - 1) {
-        setLikePost(likePostPresent);
-
-        return -1;
-      } else {
-        setLikePost(likePostPresent);
-
-        return countPresent + 1;
-      }
-    });
-  };
-
   return (
     <div className='c-joker-section'>
       {ready ? (
@@ -117,7 +98,7 @@ const JokerSection = () => {
           <div className='c-joker-section-content-joke'>
             {count === -1
               ? `"That's all the jokes for today! Come back another day!"`
-              : contentJoker[count].content}
+              : contentJoke[count].content}
           </div>
 
           <hr className='c-joker-section-hr' />
